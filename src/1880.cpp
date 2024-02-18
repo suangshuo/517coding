07503e649e6f2d0872e366b3c6c6e9df3b7ef214
suangshuo <suangshuo_ok@outlook.com>
2024-02-18 12:01:21 +0800

    提交1880题

---
+++ src/1880.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[10*10*10*10*10+100];
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int cnt=0,ret=0;
+    for(int i=0;i<n;i++){
+        if(cnt+a[i]<=m){
+            cnt+=a[i];
+            a[i]=0;
+        }else{
+            cnt=0;
+            ret++;
+        }
+    }
+    cout<<ret;
+
+    return 0;
+}
